fa28e9f4d118cc84340119c80c777be5e5d430a9
Cauê B. Favarim <cauebrolesef@gmail.com>
2026-08-25 14:33:26 -0300

    feat: add stacks

---
+++ 01-stacks/Stacks.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+public class Stacks {
+
+    public static void main(String[] args) {
+
+        // stack =  LIFO (Last In First Out)
+        //          stores objects into a sort of "vertical tower"
+        //          push() - adds an object to the top of the stack
+        //          pop() - removes the object from the top of the stack
+
+        Stack<String> stack = new Stack<>();
+
+        System.out.println(stack.isEmpty()); // true
+
+        stack.push("A");
+        stack.push("B");
+        stack.push("C");
+        stack.push("D");
+
+        System.out.println(stack.peek()); // D (top of the stack)
+        System.out.println(stack.search("D")); // 1 (counting from the top)
+        System.out.println(stack.search("A")); // 4
+        System.out.println(stack.search("Z")); // -1 (not found)
+
+        stack.pop();
+        stack.pop();
+
+        System.out.println(stack); // [A, B]
+    }
+}
